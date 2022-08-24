7c6a43e9bc8edd71329fe1b372a0d22038a76bd2
AnatoliyMikh <av.mikhaylenko@yandex.ru>
2022-08-24 18:59:31 +0300

    add method countstring

--- Program.cs
+++ Program.cs
@@ -1,10 +1,23 @@
 ﻿string[] Array = { "hello", "2", "world", ":-)", "1234", "1567" };
 
-// Метод для печати массива
-string PrintArray(string[] inputArray)
+string PrintArray(string[] inputArray)// Метод для печати массива
 {
     return string.Join(", ", inputArray);
 }
 
+int CountString(string[] input) // Метод пересчета количества строк длиной менее либо равной 3 символам
+{
+    int dl = Array.Length;
+    int count = 0;
+    int maxLengthString = 3;
+    for (int i = 0; i < dl; i++)
+    {
+        if (Array[i].Length <= maxLengthString)
+        {
+            count++;
+        }
+    }
+    return count;
+}
 
 PrintArray(Array);
